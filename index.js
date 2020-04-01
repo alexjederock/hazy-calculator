2658710a09eb9aabcb5bb1e317b932c69e1522db
alexjederock <60864509+alexjederock@users.noreply.github.com>
2020-04-01 14:57:16 -0400

    set up calculate input filtering

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+// function checkNumber(arr) {
+//     for(let i = 0; i < arr.length; i++) {
+//         if(!isNaN(arr[i])) {
+//         
+//         }
+//     }
+// }
+
+// function checkOperand(arr) {
+
+//     for(let i = 0; i < arr.length; i++) {
+//         let operand = ['+','-','*','/']
+//         if (operand.includes(arr[i + 1])) {
+//    
+//         }
+//     }
+// }
+
+//if (isNaN(arr[i + 1])) || 
+
+//typeof
+
+
+function acceptNull() {
+    if (arr.includes[null]) {
+
+    }
+}
+
+function acceptUndef() {
+    let undef = `' '`
+    if (arr.includes(undef)) {
+        return undefined
+    }
+}
+
+
+
+
+const calculate = (arr) {
+    const operands = ['+', '-', '/', '*']
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.includes([operands]) && arr.includes(!isNaN(arr[i])) {
+            return eval(arr)
+        } else if (arr.includes(arr[null]) {
+            let null = 0
+            return eval(arr)
+
+        }
+    }
+
+
+
+}
